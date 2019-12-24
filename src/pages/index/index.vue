6dd097a1336f79e4bd76d136405d5158260a16f8
jhao96 <1150307149@qq.com>
2019-12-24 10:37:24 +0800

    上传iconfont

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,52 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+  <view class="content">
+    <!-- 搜索 -->
+    <view class="search">
+      <view class="input-box">
+        <input type="text" placeholder="搜索" />
+      </view>
+      <view class="content"></view>
+    </view>
+    <!-- 焦点图 -->
+    <view class="slider"></view>
+    <!-- 主导航 -->
+    <view class="navs"></view>
+    <!-- 楼层 -->
+    <view class="floors"></view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+  data() {
+    return {
+      title: "Hello",
+      list: ["html", "css", "java"]
+    };
+  },
+  onLoad() {
+    console.log("当前页面加载了");
+  },
+  methods: {
+    btn(res) {
+      this.title = "你好";
+      console.log(res);
+    }
+  }
+};
 </script>
 
-<style>
-	.content {
+<style lang="less">
+.search {
+  .input-box {
+    padding: 20rpx 16rpx;
+	background-color: #eb4450;
+	input{
+		background-color: #fff;
+		border-radius: 10rpx;
+		height: 60rpx;
 		text-align: center;
-		height: 400upx;
 	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+  }
+}
 </style>
